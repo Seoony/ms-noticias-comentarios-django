eac9e7a9b26319f865f0a5da1c27b27d0ae2f2f7
Anthony Rivas <antho.snsd@gmail.com>
2024-05-16 09:21:51 -0500

    fix2

--- comentarios/urls.py
+++ comentarios/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('comentarios/', views.get_comentarios),
-    path('comentarios/<str:pk>/', views.get_comentario),
+    path('comentarios/<int:pk>/', views.get_comentario),
     path('comentarios/create/', views.create_comentario),
-    path('comentarios/update/<str:pk>/', views.update_comentario),
-    path('comentarios/delete/<str:pk>/', views.delete_comentario),
+    path('comentarios/update/<int:pk>/', views.update_comentario),
+    path('comentarios/delete/<int:pk>/', views.delete_comentario),
 ]
--- noticias/urls.py
+++ noticias/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   path('noticias/', views.get_noticias),
-  path('noticias/<str:pk>/', views.get_noticia),
+  path('noticias/<int:pk>/', views.get_noticia),
   path('noticias/create/', views.create_noticia),
-  path('noticias/update/<str:pk>/', views.update_noticia),
-  path('noticias/delete/<str:pk>/', views.delete_noticia),
+  path('noticias/update/<int:pk>/', views.update_noticia),
+  path('noticias/delete/<int:pk>/', views.delete_noticia),
 ]
